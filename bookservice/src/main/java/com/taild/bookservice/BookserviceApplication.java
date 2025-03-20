2f0b4ec6c7768fdbae0cc6eb4fcf4d7c7a7c5516
ductai <lductai98@gmail.com>
2025-03-20 14:39:39 +0700

    add axon server docker-compose

--- bookservice/src/main/java/com/taild/bookservice/BookserviceApplication.java
+++ bookservice/src/main/java/com/taild/bookservice/BookserviceApplication.java
@@ -2,8 +2,10 @@ package com.taild.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookserviceApplication {
 
 	public static void main(String[] args) {
