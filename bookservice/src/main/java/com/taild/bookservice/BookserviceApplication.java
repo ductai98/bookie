cf53e90f453e62ac6cd0ef37c7529e666d370fb6
ductai <lductai98@gmail.com>
2025-03-21 22:45:14 +0700

    add default exception handler

--- bookservice/src/main/java/com/taild/bookservice/BookserviceApplication.java
+++ bookservice/src/main/java/com/taild/bookservice/BookserviceApplication.java
@@ -3,9 +3,11 @@ package com.taild.bookservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.taild.bookservice", "com.taild.commonservice"})
 public class BookserviceApplication {
 
 	public static void main(String[] args) {
--- bookservice/src/main/java/com/taild/bookservice/command/model/BookRequestModel.java
+++ bookservice/src/main/java/com/taild/bookservice/command/model/BookRequestModel.java
@@ -1,5 +1,7 @@
 package com.taild.bookservice.command.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -7,7 +9,11 @@ public class BookRequestModel {
 
     private String id;
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters long")
     private String name;
+
+    @NotBlank(message = "Author is required")
 
     private String author;
 
