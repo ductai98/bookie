a2705aaf34f35c40813dc41829b324b87ff8294c
taild5 <taild5@fpt.com>
2025-03-31 00:16:48 +0700

    add notification service

--- apigateway/src/main/java/com/taild/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/taild/apigateway/ApigatewayApplication.java
@@ -3,6 +3,11 @@ package com.taild.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +15,14 @@ public class ApigatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+	@Bean
+	public KeyResolver keyResolver() {
+		return exchange -> Mono.just(
+				Objects.requireNonNull(
+						exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress()
+		);
 	}
 
 }
