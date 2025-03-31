75297659ff76c1f2f1e0f9e4adbbe9437e437391
taild5 <taild5@fpt.com>
2025-04-01 00:42:23 +0700

    config kafka bean

--- bookservice/src/main/java/com/taild/bookservice/query/controller/BookQueryController.java
+++ bookservice/src/main/java/com/taild/bookservice/query/controller/BookQueryController.java
@@ -4,13 +4,11 @@ package com.taild.bookservice.query.controller;
 import com.taild.bookservice.query.model.BookResponseModel;
 import com.taild.bookservice.query.queries.GetAllBooksQuery;
 import com.taild.bookservice.query.queries.GetBookByIdQuery;
+import com.taild.commonservice.services.KafkaService;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +18,9 @@ public class BookQueryController {
 
     @Autowired
     private QueryGateway queryGateway;
+
+    @Autowired
+    private KafkaService kafkaService;
 
     @GetMapping
     public List<BookResponseModel> getAllBooks() {
@@ -33,6 +34,11 @@ public class BookQueryController {
         GetBookByIdQuery query = new GetBookByIdQuery(id);
 
         return queryGateway.query(query, ResponseTypes.instanceOf(BookResponseModel.class)).join();
+    }
+
+    @PostMapping("/sendMessage")
+    public void sendMessage(@RequestBody String message) {
+        kafkaService.sendMessage("test", message);
     }
 
 }
---
+++ commonservice/src/main/java/com/taild/commonservice/configuration/KafkaConfig.java
@@ -0,0 +1,65 @@
+package com.taild.commonservice.configuration;
+
+import jakarta.annotation.PostConstruct;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String consumerGroupId;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProperties = new HashMap<>();
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configProperties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> configProperties = new HashMap<>();
+        configProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProperties.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+        configProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configProperties);
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bootstrap Servers: " + bootstrapServers);
+        System.out.println("Consumer Group ID: " + consumerGroupId);
+    }
+}
