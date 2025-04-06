f25252fb1d5468d540688d0120034a4cbab0efdc
taild5 <taild5@fpt.com>
2025-04-06 10:47:37 +0700

    create borrowing service

---
+++ commonservice/src/main/java/com/taild/commonservice/services/EmailService.java
@@ -0,0 +1,58 @@
+package com.taild.commonservice.services;
+
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
+@Slf4j
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+
+    /**
+     * Send email with optional attachments
+     * @param to email address
+     * @param subject of the email
+     * @param body of the email
+     * @param isHtml true if email body is HTML
+     * @param attachments optional attachments
+     */
+
+
+    public void sendEmail(String to, String subject, String body, boolean isHtml, File... attachments) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, isHtml);
+
+
+            if (attachments!= null) {
+                for (File attachment : attachments) {
+                    helper.addAttachment(attachment.getName(), attachment);
+                }
+            }
+
+
+            mailSender.send(message);
+
+            log.info("Email sent to: {}, subject: {}", to, subject);
+        } catch (MessagingException e) {
+            log.error("Error sending email: {}", e.getMessage());
+
+            //TODO Handle retry logic
+        }
+    }
+}
--- notificationservice/src/main/java/com/taild/notificationservice/event/EventConsumer.java
+++ notificationservice/src/main/java/com/taild/notificationservice/event/EventConsumer.java
@@ -1,15 +1,49 @@
 package com.taild.notificationservice.event;
 
+import com.taild.commonservice.services.EmailService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.errors.RetriableException;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.DltStrategy;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class EventConsumer {
 
+
+    private final EmailService emailService;
+
+    public EventConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @RetryableTopic(
+            attempts = "4", // 3 topics retry + 1 topic DLQ
+            backoff = @Backoff(delay = 1000, multiplier = 2),
+            autoCreateTopics = "true",
+            dltStrategy = DltStrategy.FAIL_ON_ERROR,
+            include = {RetriableException.class, RuntimeException.class}
+    )
     @KafkaListener(topics = "test", containerFactory = "kafkaListenerContainerFactory")
     public void listen(String message) {
         log.info("Received message='{}'", message);
+    }
+
+
+    @DltHandler
+    public void handleDLT(@Payload String message) {
+        log.error("Dead lettered message='{}'", message);
+    }
+
+    @KafkaListener(topics = "testEmail", containerFactory = "kafkaListenerContainerFactory")
+    public void listenEmail(String message) {
+        log.info("Received email='{}'", message);
+        emailService.sendEmail(message, "Test Email", "This is a test email", false);
+
     }
 }
